33ac9c264d64de270ac0c4fcf063c72f2744a393
praduman04 <kumarpraduman97@gmail.com>
2025-01-25 23:39:58 +0530

    connected mongodb atlas and created user schema

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
+import dotenv from "dotenv"
+import { connectDB } from "./utils/connectDB.js";
+dotenv.config()
 const app=express();
+connectDB()
 app.listen(3000,()=>{
     console.log("server started")
 })
---
+++ src/models/User.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+const UserSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Please enter name"],
+    },
+    username:{
+        type:String,
+        required:[true,"Please enter username"],
+        unique:[true,"Username already exists"],
+        
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter password"]
+    },
+    mobile:{
+        type:Number,
+        required:[true,"Please eneter mobile number"]
+    },
+    saved:{
+        type:[String],
+    },
+    followers:{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:"User"
+    },
+    friends:{
+        type:[mongoose.Schema.Types.ObjectId],
+        ref:"User"
+    }
+},{timestamps:true})
+export const UserModel=mongoose.model("User",UserSchema);
